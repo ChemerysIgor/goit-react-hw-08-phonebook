8b1079bac82ee7788718213cb02857465053342a
ChemerysIgor <105005063+ChemerysIgor@users.noreply.github.com>
2023-01-28 12:04:33 +0000

    Deploying to gh-pages from @ ChemerysIgor/goit-react-hw-08-phonebook@b2fec05c63a9c675e2b6f8d76bd2b8bfca2bc5eb 🚀

---
+++ static/js/544.e426abba.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,r){r.r(t),r.d(t,{default:function(){return l}});var n=r(6907),c=r(3329);function l(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(n.ql,{children:(0,c.jsx)("title",{children:"Home"})}),"Home"]})}}}]);
+//# sourceMappingURL=544.e426abba.chunk.js.map
